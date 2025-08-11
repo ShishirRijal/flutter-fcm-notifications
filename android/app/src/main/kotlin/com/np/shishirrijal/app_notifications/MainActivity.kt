e72e4192b31f26f390086b813e81ee80c6569468
Shishir Rijal <ccrrizal0892@gmail.com>
2025-08-11 15:13:50 +0545

    init

---
+++ android/app/src/main/kotlin/com/np/shishirrijal/app_notifications/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.np.shishirrijal.app_notifications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/home/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Home Page!',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
